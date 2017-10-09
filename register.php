e80e1f4b942ea62bd88c29375cbd908ecb8b5814
Jason Hu <hujasonx@gmail.com>
2017-10-09 19:09:35 -0400

    php work

--- register.php
+++ register.php
@@ -97,30 +97,28 @@
 			            $message = "Invalid email or password!!";
 			        }
 			    }
-			    elseif($_POST['action']=="signup")
+			    elseif($_POST['action']=="register")
 			    {
-			        $name       = mysqli_real_escape_string($connection,$_POST['name']);
 			        $email      = mysqli_real_escape_string($connection,$_POST['email']);
 			        $password   = mysqli_real_escape_string($connection,$_POST['password']);
-			        $query = "SELECT email FROM users where email='".$email."'";
+			        $query = "SELECT email FROM customers where email='".$email."'";
 			        $result = mysqli_query($connection,$query);
 			        $numResults = mysqli_num_rows($result);
 			        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
 			        {
-			            $message =  "Invalid email address please type a valid email!!";
+			            $message =  "Invalid email, address please type a valid email!!";
 			        }
 			        elseif($numResults>=1)
 			        {
-			            $message = $email." Email already exist!!";
+			            $message = $email."Email already registered.";
 			        }
 			        else
 			        {
-			            mysql_query("insert into users(name,email,password) values('".$name."','".$email."','".md5($password)."')");
+			            mysql_query("insert into customers(email,password) values('".$email."','".md5($password)."')");
 			            $message = "Signup Sucessfully!!";
 			        }
 			    }
 			}
-
 			?>
 
 
