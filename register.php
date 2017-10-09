3aafcc983e7a8bb8d5a6b25d69c112e399566e21
2changs <diana.chang05@gmail.com>
2017-10-09 18:59:54 -0400

    removed username field

--- register.php
+++ register.php
@@ -50,10 +50,6 @@
                                 </p>
                                 <form method="post" action="">
                                     <div class="field">
-                                        <label for="name">Username</label>
-                                        <input type="text" name="name" id="name" />
-                                    </div>
-                                    <div class="field">
                                         <label for="email">Email</label>
                                         <input type="email" name="email" id="email" />
                                     </div>
